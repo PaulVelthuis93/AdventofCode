fd6b90a1e37cdcbeff1168ca505091349ed01aa9
Paul Velthuis <paulvelthuis93@gmail.com>
2018-12-01 21:24:26 +0100

    task2 scala

--- src/main/scala/day1.scala
+++ src/main/scala/day1.scala
@@ -1,3 +1,5 @@
+import scala.collection.mutable
+
 object Day1 {
 
   lazy val input: String = io.Source.fromInputStream(getClass.getResourceAsStream("files/inputday1.txt")).mkString.trim
@@ -5,8 +7,32 @@ object Day1 {
 
   def resultingFreq(freqChanges: Seq[Int]): Int = freqChanges.sum
 
+
+  /**
+    * find the first frequency that occurs twice
+    *
+    * @param freqChanges
+    * @return the frequency that is found to be occuring twice, or none if it is not found
+    */
+  def firstTwiceFrequency(freqChanges: Seq[Int]): Option[Int] = {
+
+    val it = Iterator.continually(freqChanges).flatten
+    val runningtotal = it.scanLeft(0)(_ + _)
+    val prevFreqs = mutable.Set[Int]()
+    var firstTwice: Option[Int] = None
+    do {
+      val freq = runningtotal.next()
+      //      add returns false if element already in set
+      if (!prevFreqs.add(freq))
+        firstTwice = Some(freq)
+    } while (firstTwice.isEmpty)
+    firstTwice
+
+  }
+
   def main(args: Array[String]): Unit = {
     println(resultingFreq(frequencies))
+    println(firstTwiceFrequency(frequencies))
   }
 }
 
